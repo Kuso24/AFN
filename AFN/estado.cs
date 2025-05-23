18f745443c41cd01408a8f45891de53f2ffdec99
Orlando Miguel Llave Ari <stonkill2012@gmail.com>
2025-05-22 23:22:07 -0400

    AFN Orlando Miguel Llave Ari

---
+++ AFN/estado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFN
+{
+    enum estados
+    {
+        q0,
+        q1,
+        q2,
+        q3,
+        q4,
+    }
+}
